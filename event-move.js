df6cc25bd21a8d2722f630c1ae9bdac14c2ec56e
Himanshu Raj <himanshurajverma549@gmail.com>
2022-03-02 14:33:08 +0530

    improved code to resolve spacing issue

--- event-move.js
+++ event-move.js
@@ -27,7 +27,7 @@ class ANIMATETABS {
                     if(tab.parentElement != target){
                         getAllTabs[index].setAttribute("isactive", false);
                         (window.screen.width > 766)&&gsap.to(getAllTabs[index], { gridRowGap: "0", ease: "circ.out", duration: 0.2, });
-                        gsap.to(tab, { height: "0", ease: "circ.out", duration: 0.2, });
+                        gsap.to(tab, { height: "0", display:"none", ease: "circ.out", duration: 0.2, });
                         gsap.to(closeIcon[index], { display: "none", ease: "circ.out", duration: 0, });
                         gsap.to(openIcon[index], { display: "block", ease: "circ.out", duration: 0, });
                     }
@@ -40,7 +40,7 @@ class ANIMATETABS {
             if (this.tabItem.getAttribute("isactive") == "true" && this.checkFirst != "0") {
                 this.tabItem.setAttribute("isactive", false);
                 (window.screen.width > 766)&&gsap.to(this.tabItem, { gridRowGap: "0", ease: "circ.out", duration: 0.2, });
-                gsap.to(this.toOpenCloseWrp, { height: "0", ease: "circ.out", duration: 0.2, });
+                gsap.to(this.toOpenCloseWrp, { height: "0", display:"none", ease: "circ.out", duration: 0.2, });
                 gsap.to(this.closeIcon, { display: "none", ease: "circ.out", duration: 0, });
             }
             else {
@@ -55,14 +55,14 @@ class ANIMATETABS {
                 this.resetTab(e.currentTarget);
                 this.tabItem.setAttribute("isactive", true);
                 (window.screen.width > 766)&&gsap.to(e.currentTarget, { gridRowGap: this.gridGap, ease: "circ.out", duration: 0.2, });
-                gsap.to(this.toOpenCloseWrp, { height: this.increaseHeight, ease: "circ.out", duration: 0.2, });
+                gsap.to(this.toOpenCloseWrp, { height: this.increaseHeight, display:"flex", ease: "circ.out", duration: 0.2, });
                 gsap.to(this.closeIcon, {display: "block", ease: "circ.out", duration: 0,});
                 gsap.to(this.openIcon, { display: "none", ease: "circ.out", duration: 0, });
             }
             else if(this.tabItem.getAttribute("isactive") == "true"){
                 this.tabItem.setAttribute("isactive", false);
                 (window.screen.width > 766)&&gsap.to(e.currentTarget, { gridRowGap: 0, ease: "circ.out", duration: 0.2, });
-                gsap.to(this.toOpenCloseWrp, { height: 0, ease: "circ.out", duration: 0.2, });
+                gsap.to(this.toOpenCloseWrp, { height: 0, display:"none", ease: "circ.out", duration: 0.2, });
                 gsap.to(this.openIcon, { display: "block", ease: "circ.out", duration: 0, });
                 gsap.to(this.closeIcon, {display: "none", ease: "circ.out", duration: 0,});
             }
