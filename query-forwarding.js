34715d91e6eb4bef9e36244359675d41d25c7a0d
littlebigthings-dev <65065269+littlebigthings-dev@users.noreply.github.com>
2022-09-20 03:56:21 +0530

    Change for hash

--- query-forwarding.js
+++ query-forwarding.js
@@ -26,7 +26,7 @@ $('a').each(function() {
     if(href && params // 👈 null and undefined check
     && Object.keys(params).length != 0
     && Object.getPrototypeOf(params) === Object.prototype
-    && href.indexOf('#') < 0){
+    && !href.hash){
         // if existing url doesn't have 
         // if()
         // console.log(querystring);
